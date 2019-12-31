2f8f6850f4819bae55726e093786fe61a3364c93
Mal Jeff <git.hub@malcolmjeff.co.uk>
2019-12-31 13:13:43 +0000

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there! What are you doing? Bizarre!!
+          New change for the feature branch.
         </p>
         <a
           className="App-link"
